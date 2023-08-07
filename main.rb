6303c159d0d213eed7176c31dfc227ff2c656870
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-07 09:44:50 -0600

    Fix linters

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
-puts 'Hello World'
+require_relative 'menu'
+require_relative 'library_operations'
+
+def main
+  operations = LibraryOperations.new
+
+  loop do 
+    print_menu
+    selection = gets.chomp.to_i
+    operations.execute_option(selection)
+  end
+end
+
+main
