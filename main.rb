7d819e34484c15625313d27bddce44b7ff62fef0
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-07 09:45:12 -0600

    Fix linters

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative 'library_operations'
 def main
   operations = LibraryOperations.new
 
-  loop do 
+  loop do
     print_menu
     selection = gets.chomp.to_i
     operations.execute_option(selection)
