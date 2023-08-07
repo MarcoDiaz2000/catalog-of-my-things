b000e45ca518fd6a58533cecf75b0858178de460
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-07 17:53:57 +0330

    Create the Item class and add all Item class properties and methods.

---
+++ item.rb
@@ -0,0 +1,32 @@
+class Item
+  attr_reader :id, :archived
+  attr_accessor :published_date, :genre, :author, :source, :label
+
+  def initialize(published_date = nil, archived: false)
+    @id = generate_id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @published_date = published_date
+    @archived = archived
+  end
+
+  def can_be_archived?
+    return false unless published_date && published_date < 10.years.ago
+
+    true
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
+
+  private
+
+  def generate_id
+    rand(10..300)
+  end
+end
