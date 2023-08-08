7af3dc408908e568edd17c58ea33b57ab8c9c858
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-08 13:42:36 +0330

    Fix the linters.

--- label.rb
+++ label.rb
@@ -1,25 +1,24 @@
 class Label
-    attr_reader :id, :title, :color
-    attr_accessor :items
-  
-    def initialize(title, color)
-      @id = generate_id
-      @title = title
-      @color = color
-      @items = []
-    end
-  
-    def add_item(item)
-      return unless item.is_a?(Item)
-  
-      @items << item
-      item.label = self
-    end
-  
-    private
-  
-    def generate_id
-      rand(1000..9999)
-    end
+  attr_reader :id, :title, :color
+  attr_accessor :items
+
+  def initialize(title, color)
+    @id = generate_id
+    @title = title
+    @color = color
+    @items = []
   end
-  
+
+  def add_item(item)
+    return unless item.is_a?(Item)
+
+    @items << item
+    item.label = self
+  end
+
+  private
+
+  def generate_id
+    rand(1000..9999)
+  end
+end
