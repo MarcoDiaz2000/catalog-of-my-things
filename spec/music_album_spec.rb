308c85171713ae9c514d2dfef51df86b5d29326f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-08-08 10:11:42 -0600

    Fix rspec

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,29 +3,20 @@ require_relative '../item'
 require_relative '../music_album'
 
 describe MusicAlbum do
-  before(:each) do
-    @music_album = MusicAlbum.new(Time.now - (11 * 365 * 24 * 60 * 60), on_spotify: true)
-  end
-
-  describe '#initialize' do
-    it 'initializes with on_spotify attribute' do
-      expect(@music_album.on_spotify).to be true
-    end
-  end
-
   describe '#can_be_archived?' do
-    it 'returns true if parent method is true and on_spotify is true' do
-      expect(@music_album.can_be_archived?).to be true
+    it 'returns true if published_date is more than 10 years ago and on_spotify is true' do
+      music_album = MusicAlbum.new(Date.today.prev_year(11), on_spotify: true)
+      expect(music_album.can_be_archived?).to be true
     end
 
     it 'returns false if on_spotify is false' do
-      @music_album.on_spotify = false
-      expect(@music_album.can_be_archived?).to be false
+      music_album = MusicAlbum.new(Date.today.prev_year(11), on_spotify: false)
+      expect(music_album.can_be_archived?).to be false
     end
 
     it 'returns false if published_date is less than 10 years ago' do
-      @music_album.published_date = Time.now - (9 * 365 * 24 * 60 * 60)
-      expect(@music_album.can_be_archived?).to be false
+      music_album = MusicAlbum.new(Date.today.prev_year(9), on_spotify: true)
+      expect(music_album.can_be_archived?).to be false
     end
   end
 end
