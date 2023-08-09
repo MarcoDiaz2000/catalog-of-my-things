b6d6adc15990083e1178265242b33b881b443fe2
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-09 09:30:45 +0330

    Add test file for Label class.

---
+++ spec/label_spec.rb
@@ -0,0 +1,24 @@
+require_relative './spec_helper'
+
+describe Label do
+  before :each do
+    @label = Label.new('Test Label', 'red')
+  end
+
+  context '#add_item' do
+    it 'adds an item to the label' do
+      item = Item.new(Date.today - 365, archived: false)
+      @label.add_item(item)
+
+      expect(@label.items).to include(item)
+      expect(item.label).to eq 'Test Label'
+    end
+
+    it 'does not add non-Item objects to the label' do
+      non_item = 'Not an item'
+      @label.add_item(non_item)
+
+      expect(@label.items).to be_empty
+    end
+  end
+end
