c11633ad473c812ded8f3868414cf37e2636095b
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-10 07:17:30 +0330

    Add item's to_hash and from_hash methods.

--- item.rb
+++ item.rb
@@ -31,4 +31,28 @@ class Item
   def generate_id
     rand(10..300)
   end
+
+  public
+
+  def to_hash
+    {
+      id: @id,
+      genre: @genre,
+      author: @author,
+      source: @source,
+      label: @label,
+      published_date: @published_date,
+      archived: @archived
+    }
+  end
+
+  def self.from_hash(hash)
+    new_item = Item.new(hash[:published_date], archived: hash[:archived])
+    new_item.id = hash[:id]
+    new_item.genre = hash[:genre]
+    new_item.author = hash[:author]
+    new_item.source = hash[:source]
+    new_item.label = hash[:label]
+    new_item
+  end
 end
