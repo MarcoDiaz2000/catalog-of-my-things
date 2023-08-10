6003d99ef5a056756a90bf0b301b5a09bbfa6adb
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-10 16:22:54 +0330

    Update the Book class for preserving data.

--- book.rb
+++ book.rb
@@ -1,15 +1,25 @@
-# book.rb
-
 require_relative 'item'
 
 class Book < Item
   attr_accessor :title, :publisher, :cover_state
 
+  # rubocop:disable Lint/UnusedMethodArgument
   def initialize(title, publisher, cover_state, published_date = nil, archived: false)
-    super(published_date, archived: archived)
+    super(published_date)
     @title = title
     @publisher = publisher
     @cover_state = cover_state
+  end
+  # rubocop:enable Lint/UnusedMethodArgument
+
+  def to_hash
+    {
+      id: @id,
+      title: @title,
+      publisher: @publisher,
+      cover_state: @cover_state,
+      published_date: @published_date
+    }
   end
 
   private
@@ -18,25 +28,5 @@ class Book < Item
     return true if super || @cover_state == 'bad'
 
     false
-  end
-
-  public
-
-  def to_hash
-    {
-      title: @title,
-      publisher: @publisher,
-      cover_state: @cover_state
-    }.merge(super)
-  end
-
-  def self.from_hash(hash)
-    new_book = Book.new(hash[:title], hash[:publisher], hash[:cover_state], hash[:published_date], archived: hash[:archived])
-    new_book.id = hash[:id]
-    new_book.genre = hash[:genre]
-    new_book.author = hash[:author]
-    new_book.source = hash[:source]
-    new_book.label = hash[:label]
-    new_book
   end
 end
